727d6a0435342c9973e495ce8e5d6be2841668e7
Антон Власов <druidvav@gmail.com>
2018-06-20 21:50:24 +0300

    facebook api version bump

--- src/Druidvav/SimpleOauthBundle/OAuth/ResourceOwner/FacebookResourceOwner.php
+++ src/Druidvav/SimpleOauthBundle/OAuth/ResourceOwner/FacebookResourceOwner.php
@@ -102,10 +102,10 @@ class FacebookResourceOwner extends GenericOAuth2ResourceOwner
         parent::configureOptions($resolver);
 
         $resolver->setDefaults(array(
-            'authorization_url' => 'https://www.facebook.com/v2.8/dialog/oauth',
-            'access_token_url' => 'https://graph.facebook.com/v2.8/oauth/access_token',
-            'revoke_token_url' => 'https://graph.facebook.com/v2.8/me/permissions',
-            'infos_url' => 'https://graph.facebook.com/v2.8/me?fields=first_name,last_name,name,email',
+            'authorization_url' => 'https://www.facebook.com/v2.12/dialog/oauth',
+            'access_token_url' => 'https://graph.facebook.com/v2.12/oauth/access_token',
+            'revoke_token_url' => 'https://graph.facebook.com/v2.12/me/permissions',
+            'infos_url' => 'https://graph.facebook.com/v2.12/me?fields=first_name,last_name,name,email',
             'use_commas_in_scope' => true,
             'display' => null,
             'auth_type' => null,
